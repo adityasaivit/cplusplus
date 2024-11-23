12cdb0f8f21231cba5fee23c2f33f8176eef7de6
adityasaivit <113006142+adityasaivit@users.noreply.github.com>
2024-11-23 22:05:45 +0530

    Add files via upload

---
+++ getuserinput.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+int main()
+{
+
+	// get input
+
+	// how to get input
+
+
+	// we have to store the information feom the user
+	string name;
+	getline(cin,name);
+	cout<<"the name is "<<name<<endl;
+	int age;
+
+	double d;
+	cout<<"enter the double type age:";
+	cin>>d;
+	cout<<"the output is "<<d<<endl;
+
+
+	cout<<"enter the age:";
+	cin>>age;
+
+	cout<<"the age of the person entered:"<<age<<endl;
+
+	// take all things and print the output
+
+	// to take the input of the stirng we can use the getline() function instead of the cin
+
+	// getline() will take the spaces into the consideration and doesnot stop after encountering it
+	// until enter key
+
+
+
+
+
+	return 0;
+
+}
