3d74bca662483e75d2c8108f194a7b55594bc9e5
adityasaivit <113006142+adityasaivit@users.noreply.github.com>
2024-11-23 22:05:13 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+int main()
+{
+    int n1,n2;
+
+    cout<<"enter the number1:"<<endl;
+    cin>>n1;
+
+    cout<<"enter the number2:"<<endl;
+    cin>>n2;
+
+    int sum=n1+n2;
+    cout<<"the sum is "<<sum<<endl;
+
+    int sub=n1-n2;
+    cout<<"the difference is "<<sub<<endl;
+
+    int mul=n1*n2;
+    cout<<"the multiplication is "<<mul<<endl;
+
+    int div=n1/n2;
+    cout<<"the division is"<<div<<endl;
+
+    return 0;
+}
