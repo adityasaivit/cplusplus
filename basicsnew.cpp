ce3b9b504c5e8b132552e7044a6ca9bab1768ea5
adityasaivit <113006142+adityasaivit@users.noreply.github.com>
2024-11-23 16:52:24 +0530

    Add files via upload

---
+++ basicsnew.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	cout<<"hello world!"<<endl;
+
+	string a="this is aditya";
+	cout<<a<<endl;
+	string name="Aditya Sai";
+
+	int age=22;
+	// for stirng we should surround them with the quotes
+
+	cout<<"hello"<<" "<<name<<endl;
+	cout<<"my age is "<<age<<endl;
+	cout<<"the age is "<<age<<" years old"<<endl;
+
+
+	
+
+
+
+
+
+}
+
+// we are going to build the program and then run the program
+// we tell our computer what to do
+// running is different from building it 
+
+// main() is important the execution starts from the main function
+// any thing after the curly braces will be running 
+
+// cout  means console out to print in the console endl is the end of the line
+
+// program was the bunch of instructions for computer to execute something
+
+// more complex instructionf and the more complex is the program
+
+// variable is the container that stre the value and main taint it
+
+// variable major use will comes with the usability and 
+// value resuability
+
+// the variables that was used to store the various types of data
+
